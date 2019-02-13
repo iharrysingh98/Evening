d25d6634bf4288be79b797e3b754310a8caa1081
Hardeep Singh <hardeepsingh13598@gmail.com>
2019-02-13 10:06:08 +0530

    DoublyLinkedList.java

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,102 +1,50 @@
-class Node {
-	public int data;
-	public Node next;
-	public Node prev;
- 
-	public void displayNodeData() {
-		System.out.println("{ " + data + " } ");
+import java.util.Scanner;
+class node
+{
+	int data;
+	node next;
+	node prev=null;
+}
+public class DoublyLinkedList {
+	node head;
+    public void insert(int data)
+	{
+		node n=new node();
+		n.data=data;
+		if(head==null)
+		{
+			head=n;
+		}
+		else
+		{
+			node temp=head;
+			while(temp.next!=null)
+			{
+				temp=temp.next;
+			}
+			temp.next=n;
+			n.prev=temp;
+		}
+		
+	}
+	void display()
+	{
+		node temp=head;
+		while(temp.next!=null)
+		{
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+	    DoublyLinkedList l=new DoublyLinkedList();
+	    int d=s.nextInt();
+	    for(int i=0;i<d;i++)
+	    {
+	    	int a=s.nextInt();
+	    	l.insert(a);
+	    }
+	    l.display();
 	}
 }
- 
-public class DoublyLinkedList {
- 
-	private Node head;
-	private Node tail;
-	int size;
- 
-	public boolean isEmpty() {
-		return (head == null);
-	}
-	public void insertFirst(int data) {
-		Node newNode = new Node();
-		newNode.data = data;
-		newNode.next = head;
-		newNode.prev=null;
-		if(head!=null)
-			head.prev=newNode;
-		head = newNode;
-		if(tail==null)
-			tail=newNode;
-		size++;
-	}
- 
-	// used to insert a node at the start of linked list
-	public void insertLast(int data) {
-		Node newNode = new Node();
-		newNode.data = data;
-		newNode.next = null;
-		newNode.prev=tail;
-		if(tail!=null)
-			tail.next=newNode;
-		tail = newNode;
-		if(head==null)
-			head=newNode;
-		size++;
-	}
-	
-	public Node deleteFirst() {
- 
-		if (size == 0) 
-			throw new RuntimeException("Doubly linked list is already empty");
-		Node temp = head;
-		head = head.next;
-		head.prev = null;
-		size--;
-		return temp;
-	}
- 
-	// used to delete node from last of Doubly linked list
-	public Node deleteLast() {
- 
-		Node temp = tail;
-		tail = tail.prev;
-		tail.next=null;
-		size--;
-		return temp;
-	}
- 
- 
-	
-	public void deleteAfter(Node after) {
-		Node temp = head;
-		while (temp.next != null && temp.data != after.data) {
-			temp = temp.next;
-		}
-		if (temp.next != null)
-			temp.next.next.prev=temp;
-		temp.next = temp.next.next;
- 
-	}
- 
-	
-	public void printLinkedListForward() {
-		System.out.println("Printing Doubly LinkedList (head --> tail) ");
-		Node current = head;
-		while (current != null) {
-			current.displayNodeData();
-			current = current.next;
-		}
-		System.out.println();
-	}
- 
-	
-	public void printLinkedListBackward() {
-		System.out.println("Printing Doubly LinkedList (tail --> head) ");
-		Node current = tail;
-		while (current != null) {
-			current.displayNodeData();
-			current = current.prev;
-		}
-		System.out.println();
-	}
-} 
